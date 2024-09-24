b28e781f8f7dcd4f195c2c1a97efb3d64ed21390
marianotravella <marianotravella2@gmail.com>
2024-09-24 18:19:30 -0300

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Common.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace GarageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginDto loginData)
+        {
+
+            if (loginData.UserName == null || loginData.UserName == "")//es lo mismo que: if(string.IsNullOrEmpty(loginData.UserName))
+            {
+                return BadRequest("Debe ingresar un nombre de usuario.");
+            }
+            if (loginData.Password == null || loginData.Password == "")//es lo mismo que: if(string.IsNullOrEmpty(loginData.Password))
+            {
+                return BadRequest("Debe ingresar una contraseña.");
+            }
+
+            var loginResult = _userService.ValidateUser(loginData);
+
+            if (loginResult == true)
+            {
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+
+using Data;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Services;
+
+
+namespace GarageApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationContext>(dbContextOptions => dbContextOptions.UseSqlite(
+                builder.Configuration["ConnectionStrings:AgendaAPIDBCConnectionString"]));
+
+            builder.Services.AddSingleton<UserRepository>();
+            builder.Services.AddSingleton<UserService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
